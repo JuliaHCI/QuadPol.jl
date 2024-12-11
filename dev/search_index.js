284f9805af2b1e4ed62b5a8221ccc255d6fc8787
Documenter.jl <documenter@juliadocs.github.io>
2024-12-11 12:58:07 +0000

    build based on 847718c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuadPol","category":"page"},{"location":"#QuadPol","page":"Home","title":"QuadPol","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuadPol.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuadPol]","category":"page"}]
+}
