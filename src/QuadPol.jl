96e7324537b9e139b2553001ec78656a8d16db2a
Miles Lucas <mdlucas@hawaii.edu>
2024-12-10 21:56:01 -1000

    add Struct for quadrants

--- src/QuadPol.jl
+++ src/QuadPol.jl
@@ -3,11 +3,24 @@ using Printf
 
 export quadpol
 
+struct Quadrants{T}
+    Q000::AbstractMatrix{T}
+    Q090::AbstractMatrix{T}
+    Q180::AbstractMatrix{T}
+    Q270::AbstractMatrix{T}
+    U045::AbstractMatrix{T}
+    U135::AbstractMatrix{T}
+    U225::AbstractMatrix{T}
+    U315::AbstractMatrix{T}
+    Qphi::AbstractMatrix{T}
+    Uphi::AbstractMatrix{T}
+end
+
 function extract_quadrants(
-    Q::AbstractMatrix,
-    U::AbstractMatrix;
+    Q::AbstractMatrix{T},
+    U::AbstractMatrix{T};
     pa,
-)
+) where {T}
     ## Step 1
     # artifically rotate the Stokes lobes by pa
     Qrot = Q * cosd(2 * pa) - U * sind(2 * pa)
@@ -51,7 +64,7 @@ function extract_quadrants(
     mask = @. 270 < disk_angles <= 360
     U315 = mask .* Urot
 
-    return (;
+    return Quadrants(
         Q000,
         Q090,
         Q180,
@@ -65,8 +78,7 @@ function extract_quadrants(
     )
 end
 
-
-function calculate_statistics(quadpols)
+function calculate_statistics(quadpols::Quadrants)
     # Quadrant sums
     Q000 = sum(quadpols.Q000)
     Q090 = sum(quadpols.Q090)
@@ -77,24 +89,30 @@ function calculate_statistics(quadpols)
     U225 = sum(quadpols.U225)
     U315 = sum(quadpols.U315)
 
-    Q_int = sum((Q000, Q090, Q180, Q270))
-    U_int = sum((U045, U135, U225, U315))
-    Q_abs_int = sum(abs, (Q000, Q090, Q180, Q270))
-    U_abs_int = sum(abs, (U045, U135, U225, U315))
+    Q_quads = (Q000, Q090, Q180, Q270)
+    U_quads = (U045, U135, U225, U315)
+    Qd = sum(Q_quads)
+    Ud = sum(U_quads)
+    Qd_abs = sum(abs, Q_quads)
+    Ud_abs = sum(abs, U_quads)
 
-    Qphi_int = sum(quadpols.Qphi)
-    Uphi_int = sum(quadpols.Uphi)
+    Qphi = sum(quadpols.Qphi)
+    Uphi = sum(quadpols.Uphi)
+    
+    left_right_Qsum = abs(Q090) + abs(Q270)
+    left_right_Utop = abs(U045) + abs(U315)
+    left_right_Ubot = abs(U135) + abs(U225)
 
-    delta_090_270 = (abs(Q090) - abs(Q270)) / (abs(Q090) + abs(Q270))
-    delta_045_315 = (abs(U045) - abs(U315)) / (abs(U045) + abs(U315))
-    delta_135_225 = (abs(U135) - abs(U225)) / (abs(U135) + abs(U225))
+    delta_090_270 = (abs(Q090) - abs(Q270)) / left_right_Qsum
+    delta_045_315 = (abs(U045) - abs(U315)) / left_right_Utop
+    delta_135_225 = (abs(U135) - abs(U225)) / left_right_Ubot
 
     lambda_000_180 = abs(Q000) / abs(Q180)
     lambda_045_135 = abs(U045) / abs(U135)
     lambda_315_225 = abs(U315) / abs(U225)
 
-    lambda_a = (abs(Q090) + abs(Q270)) / (2 * abs(Q180))
-    lambda_b = (abs(Q090) + abs(Q270)) / (abs(U135) + abs(U225))
+    lambda_a = left_right_Qsum / (2 * abs(Q180))
+    lambda_b = left_right_Qsum / left_right_Ubot
 
     return (;
         Q000,
@@ -105,12 +123,12 @@ function calculate_statistics(quadpols)
         U135,
         U225,
         U315,
-        Q_int,
-        U_int,
-        Q_abs_int,
-        U_abs_int,
-        Qphi_int,
-        Uphi_int,
+        Qd,
+        Ud,
+        Qd_abs,
+        Ud_abs,
+        Qphi,
+        Uphi,
         delta_090_270,
         delta_045_315,
         delta_135_225,
@@ -124,13 +142,13 @@ end
 
 function print_statistics(stats)
     @printf("Relative quadrant polarization statistics\n")
-    @printf("Q000/Qphi =\t%f\tU045/Qphi = \t%f\n", stats.Q000 / stats.Qphi_int, stats.U045 / stats.Qphi_int)
-    @printf("Q090/Qphi =\t%f\tU135/Qphi = \t%f\n", stats.Q090 / stats.Qphi_int, stats.U135 / stats.Qphi_int)
-    @printf("Q180/Qphi =\t%f\tU225/Qphi = \t%f\n", stats.Q180 / stats.Qphi_int, stats.U225 / stats.Qphi_int)
-    @printf("Q270/Qphi =\t%f\tU315/Qphi = \t%f\n", stats.Q270 / stats.Qphi_int, stats.U315 / stats.Qphi_int)
+    @printf("Q000/Qphi =\t%f\tU045/Qphi = \t%f\n", stats.Q000 / stats.Qphi, stats.U045 / stats.Qphi)
+    @printf("Q090/Qphi =\t%f\tU135/Qphi = \t%f\n", stats.Q090 / stats.Qphi, stats.U135 / stats.Qphi)
+    @printf("Q180/Qphi =\t%f\tU225/Qphi = \t%f\n", stats.Q180 / stats.Qphi, stats.U225 / stats.Qphi)
+    @printf("Q270/Qphi =\t%f\tU315/Qphi = \t%f\n", stats.Q270 / stats.Qphi, stats.U315 / stats.Qphi)
     @printf("\nQuadrant sum statistics\n")
-    @printf("ΣQxxx/Qphi =\t%f\tΣUxxx/Qphi =\t%f\t\n", stats.Q_int / stats.Qphi_int, stats.U_int / stats.Qphi_int)
-    @printf("Σ|Qxxx|/Qphi =\t%f\tΣ|Uxxx|/Qphi =\t%f\t\n", stats.Q_abs_int / stats.Qphi_int, stats.U_abs_int / stats.Qphi_int)
+    @printf("ΣQxxx/Qphi =\t%f\tΣUxxx/Qphi =\t%f\t\n", stats.Qd / stats.Qphi, stats.Ud / stats.Qphi)
+    @printf("Σ|Qxxx|/Qphi =\t%f\tΣ|Uxxx|/Qphi =\t%f\t\n", stats.Qd_abs / stats.Qphi, stats.Ud_abs / stats.Qphi)
     @printf("\nLeft-right asymmetry deviations\n")
     @printf("Δ090/270 =\t%f%%\tΔ045/315 =\t%f%%\t\n", stats.delta_090_270 * 100, stats.delta_045_315 * 100)
     @printf("\t\t\t\tΔ135/225 =\t%f%%\t\n", stats.delta_135_225 * 100)
--- test/test_HR4796.jl
+++ test/test_HR4796.jl
@@ -12,19 +12,19 @@ pa=-118
 
 
 cx, cy = size(Q) ./ 2 .+ 0.5
-mask_inner = EllipticalAperture(cx, cy, 16, 120, 90-pa)
+mask_inner = EllipticalAperture(cx, cy, 15, 117, 90-pa)
 mask_outer = EllipticalAperture(cx, cy, 51, 182, 90-pa)
 
 idxs = CartesianIndices(Q)
 mask = map(idx -> mask_outer[idx] - mask_inner[idx], idxs)
 
-nx, ny = size(Q)
-xs = range(-nx/2 + 0.5, nx/2 - 0.5)
-ys = range(-ny/2 + 0.5, ny/2 - 0.5)
-sky_angles = atand.(-xs, ys')
-disk_angles = mod.(sky_angles .+ pa, 360)
-quad_mask = @. (disk_angles <= 45) | (disk_angles > 315)
-@. mask *= 1 - Int(quad_mask)
+# nx, ny = size(Q)
+# xs = range(-nx/2 + 0.5, nx/2 - 0.5)
+# ys = range(-ny/2 + 0.5, ny/2 - 0.5)
+# sky_angles = atand.(-xs, ys')
+# disk_angles = mod.(sky_angles .+ pa, 360)
+# quad_mask = @. (disk_angles <= 45) | (disk_angles > 315)
+# @. mask .*= 1 - Int(quad_mask)
 
 Q_stamp = Q .* mask
 U_stamp = U .* mask
